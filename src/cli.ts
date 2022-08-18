e67c1333d932e8647e294d7806bb54569cbc637b
JacobLinCool <jacoblincool@gmail.com>
2022-08-19 03:20:14 +0800

    feat: fix dts and optionally remove browser js

--- src/cli.ts
+++ src/cli.ts
@@ -2,17 +2,21 @@ import fs from "node:fs";
 import path from "node:path";
 
 const args = process.argv.slice(2);
+const HELP = args.includes("--help") || args.includes("-h");
 const DIR = args.filter((arg) => !arg.startsWith("-"))[0] || ".";
 const DRY = args.includes("--dry") || args.includes("-d");
-const HELP = args.includes("--help") || args.includes("-h");
+const NO_FIX_DTS = args.includes("--no-fix-dts");
+const NO_BROWSER = args.includes("--no-browser");
 
 export function run(): void {
     if (HELP) {
         console.log(
             "prisma-runtime: fix require/import path of prisma generated files in a directory",
         );
-        console.log("Options: -d, --dry  : dry run");
-        console.log("         -h, --help : show this help");
+        console.log("Options: -d, --dry    : dry run");
+        console.log("         -h, --help   : show this help");
+        console.log("         --no-fix-dts : do not fix d.ts exports");
+        console.log("         --no-browser : remove index-browser.js");
         console.log("Example: prisma-runtime target/dir");
         return;
     }
@@ -35,6 +39,16 @@ export function run(): void {
     }
 
     if (!DRY) {
+        if (NO_BROWSER) {
+            for (let i = files.length - 1; i >= 0; i--) {
+                if (path.basename(files[i]) === "index-browser.js") {
+                    fs.unlinkSync(files[i]);
+                    console.log(`Removed ${files[i]}`);
+                    files.splice(i, 1);
+                }
+            }
+        }
+
         const dirs = new Set<string>();
         files.forEach((file) => {
             const content = fs.readFileSync(file, "utf8");
@@ -64,6 +78,17 @@ export function run(): void {
                 console.log(`Removed ${lib}`);
             }
         });
+
+        if (!NO_FIX_DTS) {
+            files
+                .filter((file) => file.endsWith(".d.ts"))
+                .forEach((file) => {
+                    const content = fs.readFileSync(file, "utf8");
+                    const patched = content.replace(/export import/g, "export type");
+                    fs.writeFileSync(file, patched);
+                    console.log(`DTS Patched ${file}`);
+                });
+        }
     }
 }
 
